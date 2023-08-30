08d5ced82541f5578e4e47a842af588eddd790ce
German Vazquez <german.vazquez@softtek.com>
2023-08-30 12:50:33 -0600

    Usando el WebDriverManager, eliminamos el ChromeDriver.exe

--- src/test/java/com/qualitystream/tutorial/GST.java
+++ src/test/java/com/qualitystream/tutorial/GST.java
@@ -12,6 +12,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class GST {
 	
 	private WebDriver driver;
@@ -19,7 +21,7 @@ public class GST {
 	@Before
 	public void setUp() {
 
-		System.setProperty("webdriver.chrome.driver", "./src/test/resources/drivers/chromedriver.exe");
+		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.google.com/");
